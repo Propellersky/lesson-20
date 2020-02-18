7d7a9058e3c24f68a0be9175ea710c1c0e44c582
Propellersky <propellersky@gmail.com>
2020-02-18 20:49:36 +0200

    eventListener

---
+++ script.js
@@ -0,0 +1,68 @@
+let a = 0;
+let first = document.getElementById("first");
+first.onclick = function() {
+  let on = document.getElementById("but");
+  let Text = "You clicked First button";
+  on.innerHTML = Text;
+};
+first.onmousemove = function() {
+  a++;
+  this.style.width = 100 + "px";
+  this.style.height = 60 + "px";
+  first.onmouseleave = function() {
+    a++;
+    this.style.width = 70 + "px";
+    this.style.height = 50 + "px";
+  };
+};
+let fst = document.querySelector("first");
+
+first.addEventListener("click", function() {
+  console.log("firstElement");
+});
+
+let b = 0;
+let second = document.getElementById("second");
+second.onclick = function() {
+  let on = document.getElementById("but");
+  let Text = "You clicked Second button";
+  on.innerHTML = Text;
+};
+second.onmousemove = function() {
+  b++;
+  this.style.width = 100 + "px";
+  this.style.height = 60 + "px";
+  second.onmouseleave = function() {
+    b++;
+    this.style.width = 70 + "px";
+    this.style.height = 50 + "px";
+  };
+};
+let scd = document.querySelector("second");
+
+second.addEventListener("click", function() {
+  console.log("secondElement");
+});
+
+let c = 0;
+let third = document.getElementById("third");
+third.onclick = function() {
+  let on = document.getElementById("but");
+  let Text = "You clicked Third button";
+  on.innerHTML = Text;
+};
+third.onmousemove = function() {
+  c++;
+  this.style.width = 100 + "px";
+  this.style.height = 60 + "px";
+  third.onmouseleave = function() {
+    c++;
+    this.style.width = 70 + "px";
+    this.style.height = 50 + "px";
+  };
+};
+let thd = document.querySelector("third");
+
+third.addEventListener("click", function() {
+  console.log("thirdElement");
+});
